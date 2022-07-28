ca12d28734d1eb2fe7513b53a4fab2ac7f932213
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-07-28 17:12:38 -0400

    Got it to work but still unsure
    The DOM content is stated twice and im unsure?

--- js/scripts.js
+++ js/scripts.js
@@ -38,12 +38,20 @@ return {
 };
 })();
 
-pokemonRepository.add({name: 'Shmais', height: 7, type: [Redhead]}); //adding a new pokemon to the array
+pokemonRepository.add({name: 'Shmais', height: 7, type: 'Redhead'}); //adding a new pokemon to the array
 
 console.log(pokemonRepository.getAll()); //log the array with the new item
 
-pokemonRepository.getAll().forEach(function(pokemon));{ //apply the condition and show all items
-pokemonRepository.addListItem(pokemon);
-}
+pokemonRepository.getAll().forEach(function(pokemon) {//apply the condition and show all items
+  let pokemonList = document.querySelector(".pokemon-list"); //target the class pokemon-list in the HTML file
+  let listItem = document.createElement("li"); //Creat a list itmes on the ul class pokemon-list 
+  let button = document.createElement("button"); //create buttons out of the UL list
+  button.innerText = pokemon.name; // insert the pokemon names in the inner text of the button
+  button.classList.add("button-class"); //link it to the button-class to target it in the CSS firl
+  listItem.appendChild(button); //insert the li into the button on the webpage
+  pokemonList.appendChild(listItem); //apply it to the web page
+});
+
+
 
 
