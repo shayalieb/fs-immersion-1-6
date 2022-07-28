228ffd3b0ab4c1c6e4a830b0a589c0b6f051d649
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-07-28 17:30:22 -0400

    Made a correction using the right syntax
    On the last line, instead of rewriting the whole addListItem function, i just called on it by referencing the function and than calling it in the next line

--- js/scripts.js
+++ js/scripts.js
@@ -31,25 +31,19 @@ function addListItem(pokemon){
   pokemonList.appendChild(listItem); //apply it to the web page
 };
 
-return {
-  add: add, //return the data of the added items
-  getAll: getAll, //get all the items
-  addListItem: addListItem //return the new list item
-};
-})();
+  return {
+    add: add, //return the data of the added items
+    getAll: getAll, //get all the items
+    addListItem: addListItem //return the new list item
+  };
+  })();
 
 pokemonRepository.add({name: 'Shmais', height: 7, type: 'Redhead'}); //adding a new pokemon to the array
 
 console.log(pokemonRepository.getAll()); //log the array with the new item
 
-pokemonRepository.getAll().forEach(function(pokemon) {//apply the condition and show all items
-  let pokemonList = document.querySelector(".pokemon-list"); //target the class pokemon-list in the HTML file
-  let listItem = document.createElement("li"); //Creat a list itmes on the ul class pokemon-list 
-  let button = document.createElement("button"); //create buttons out of the UL list
-  button.innerText = pokemon.name; // insert the pokemon names in the inner text of the button
-  button.classList.add("button-class"); //link it to the button-class to target it in the CSS firl
-  listItem.appendChild(button); //insert the li into the button on the webpage
-  pokemonList.appendChild(listItem); //apply it to the web page
+pokemonRepository.getAll().forEach(function(pokemon) {//Referencing the function 
+  pokemonRepository.addListItem(pokemon);//calling the addListItem, and adding (pokemon) to call the function name
 });
 
 
