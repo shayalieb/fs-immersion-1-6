60da93d17f7ab865967d6ad1cd5dbcf2efa66701
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-07-28 16:52:16 -0400

    Cleaned up and added commentary to the JS file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-//This object variable will contain the Pokemon "name", "height", and type
-
-//start the IIFE fucntion
-let pokemonRepository = (function () {
+let pokemonRepository = (function () { //Creating the array of objects
   let pokemonList = [
     {name: 'Metapod', type: 'Bug', height: 0.7}, 
     {name: 'Weedle', type: ['bug', 'Poison'], height: 0.3},
@@ -9,57 +6,44 @@ let pokemonRepository = (function () {
     {name: 'Pikachu', type: 'Electric', height: 0.4}
   ];
   
-  function add(pokemon) {//a function to add the new object
-    if (
-      typeof pokemon === 'object' && 
-      'name' in pokemon &&
-      'height' in pokemon &&
-      'type' in pokemon 
-    ) {
-    pokemonList.push(pokemon);
-  } else {
-    console.log('This is not a correct pokemon');
-  }
-}
-function getAll() {//to return the object anbd keys
-    return pokemonList;
-  }
-  //Configuring ways to create the new functions, while connecting to the HTML class=".pokemon-list", as well as creating the list items (li), and buttons 
-  //with button text and assigning a class to the buttons, and adding the names to the button text.
-function addListItem() {
-  let pokemonList = document.querySelector(".pokemon-list");
-  let listPokemon = document.createElement("li"); 
-  let pokemonButton = document.createElement("button");
-  button.innerText = pokemon.name;
-  button.classList.add("button-class");
-  pokemonList.appendChild(button);
-}
+  function add(pokemon) { //creating the function to set the loop of if's (function is called pokemon)
+    if ( 
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "type" in pokemon
+    ){
+      pokemonList.push(pokemon); //push pokemon list to the function
+    } else { // if it not on the list it should log....
+      console.log("pokemon is not on this list");
+  }  
+};
+function getAll() { //should return all that is in the variable pokemonList
+  return pokemonList;
+}; 
+function addListItem(pokemon){ 
+  let pokemonList = document.querySelector(".pokemon-list"); //target the class pokemon-list in the HTML file
+  let listItem = document.createElement("li"); //Creat a list itmes on the ul class pokemon-list 
+  let button = document.createElement("button"); //create buttons out of the UL list
+  button.innerText = pokemon.name; // insert the pokemon names in the inner text of the button
+  button.classList.add("button-class"); //link it to the button-class to target it in the CSS firl
+  listItem.appendChild(button); //insert the li into the button on the webpage
+  pokemonList.appendChild(listItem); //apply it to the web page
+};
 
-  return {//return the new object
-    add: add,
-    getAll: getAll
-  };
+return {
+  add: add, //return the data of the added items
+  getAll: getAll, //get all the items
+  addListItem: addListItem //return the new list item
+};
 })();
 
-//end the IIFE function
+pokemonRepository.add({name: 'Shmais', height: 7, type: [Redhead]}); //adding a new pokemon to the array
 
-//forEach is a pre defined function in JS. item => followed by a {} which indicates to start the function
-pokemonRepository.getAll().forEach(item => {  
-  if (item.height > 0 && item.height < 0.4) {
-      document.write('<h1>' + item.name + ' (height: ' + item.height + ') - This is a small Pokepmon!)</h1>') 
-  } else if (item.height > 0.3 && item.height < 0.7) {
-    document.write('<h1>' + item.name + ' (height: ' + item.height + ') - This is a medium sized Pokemon!)</h1>') 
-  } else {
-    document.write('<h1>' + item.name + ' (height: ' + item.height + ') - This is a large pokemon!)</h1>') 
-  }
-})
+console.log(pokemonRepository.getAll()); //log the array with the new item
 
-//This is how we would add the new item
-pokemonRepository.add({ name: 'Shmais', height: 7, type: ['Redhead']});//adding the new object
-console.log(pokemonRepository.getAll());//retrieve the new object
-
-//Execute the function to apply it to what was configured above
-pokemonRepository.getAll().forEach() 
+pokemonRepository.getAll().forEach(function(pokemon));{ //apply the condition and show all items
 pokemonRepository.addListItem(pokemon);
-pokemonRepository.appendChild
+}
+
 
